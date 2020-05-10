71bc66774d990a9de02de8495c6554696eb6554d
Michael Washington <webmaster@adefwebserver.com>
2020-05-10 15:44:46 -0700

    Re-factored to make code cleaner and allow for more examples to be added without collisions

---
+++ Classes/InteropDummy3.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorBabylonJs
+{
+    public static class InteropDummy3
+    {
+        internal static ValueTask ShowBabylon(
+            IJSRuntime jsRuntime, 
+            ElementReference CanvasElement)
+        {
+            return jsRuntime.InvokeVoidAsync(
+            "Dummy3.showBabylon",
+            CanvasElement);
+        }
+
+        internal static ValueTask Show(
+            IJSRuntime jsRuntime)
+        {
+            return jsRuntime.InvokeVoidAsync(
+            "Dummy3.showUI",
+            null);
+        }
+
+        internal static ValueTask Hide(
+            IJSRuntime jsRuntime)
+        {
+            return jsRuntime.InvokeVoidAsync(
+            "Dummy3.hideUI",
+            null);
+        }
+
+        internal static ValueTask Idle(
+            IJSRuntime jsRuntime)
+        {
+            return jsRuntime.InvokeVoidAsync(
+            "Dummy3.idleBabylon",
+            null);
+        }
+
+        internal static ValueTask Walk(
+            IJSRuntime jsRuntime)
+        {
+            return jsRuntime.InvokeVoidAsync(
+            "Dummy3.walkBabylon",
+            null);
+        }
+
+        internal static ValueTask Run(
+            IJSRuntime jsRuntime)
+        {
+            return jsRuntime.InvokeVoidAsync(
+            "Dummy3.runBabylon",
+            null);
+        }
+    }
+}
