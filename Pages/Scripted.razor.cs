e8009a5cf0bc175c65df57ed8de216ba1da4cfeb
Mister Magoo <mister.magoo+githubsql@gmail.com>
2020-07-09 01:52:31 +0100

    Default Scene builds and renders ok Look for TODO items This is still a first draft to see how things work out... Changes are very likely

---
+++ Pages/Scripted.razor.cs
@@ -0,0 +1,27 @@
+﻿using BabylonBlazor.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBabylonJs.Pages
+{
+	public partial class Scripted
+	{
+		[Inject] IJSRuntime JS { get; set; }
+		Canvas canvas;
+
+		protected override async Task OnAfterRenderAsync(bool firstRender)
+		{
+			if (firstRender)
+			{
+				var engine = canvas.GetEngine();
+				var scene = await engine.WithDefaultScene().Build();
+				await scene.Render();
+			}
+		}
+	}
+}
